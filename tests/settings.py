60c86356ff6006ad36db4842a1fead8060919654
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2025-03-28 11:25:10 +0100

    Remove coding preambules and __future__ imports.

--- tests/settings.py
+++ tests/settings.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8
-from __future__ import absolute_import, unicode_literals
-
 DEBUG = False
 USE_TZ = True
 
--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 """
 Tests for `django-allow-cidr` middleware.
 """
