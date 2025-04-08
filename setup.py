134e2c498a3165c768a1cd3ac483e4464adcbae9
Jan Brasna <1784648+janbrasna@users.noreply.github.com>
2025-04-08 10:24:05 +0200

    Revert classifiers

--- setup.py
+++ setup.py
@@ -44,6 +44,9 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Framework :: Django",
+        "Framework :: Django :: 4.2",
+        "Framework :: Django :: 5.1",
+        "Framework :: Django :: 5.2",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
